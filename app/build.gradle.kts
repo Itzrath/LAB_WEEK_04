b3962e9b2de819ebd08466bbd7e93a630ea1e823
ArrathG <saduady@gmail.com>
2025-09-19 21:21:52 +0700

    Commit No. 2 – Bottom Navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
